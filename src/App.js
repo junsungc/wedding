c309f53723fe828a9781bfd27a56f280f877a6b3
hnc-cjs <cjs@hancom.com>
2024-01-18 16:36:58 +0900

    청첩창 페이지 step 1 : 기본 images 틀 잡기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("hii")
+  console.log(process.env.PUBLIC_URL)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* 사진 추가 */}
+      <div className="image-wrapper">
+        <img src={'/images/1.png'} alt="Image 1" />
+      </div>
+      <div className="image-wrapper">
+        <img src={'/images/2.png'} alt="Image 1" />
+      </div>
+      <div className="image-wrapper">
+        <img src={'/images/3.png'} alt="Image 1" />
+      </div>
+      <div className="image-wrapper">
+        <img src={'/images/4.png'} alt="Image 1" />
+      </div>
+      <div className="image-wrapper">
+        <img src={'/images/5.png'} alt="Image 1" />
+      </div>
+      <div className="image-wrapper">
+        <img src={'/images/6.png'} alt="Image 1" />
+      </div>
+      <div className="image-wrapper">
+        <img src={'/images/7.png'} alt="Image 1" />
+      </div>
+      <div className="image-wrapper">
+        <img src={'/images/8.png'} alt="Image 1" />
+      </div>
+      {/* 추가적인 사진들도 유사하게 추가 가능 */}
     </div>
   );
 }
