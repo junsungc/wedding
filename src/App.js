46df4497485a047b50b3195296828879f18e9044
test <test@email.com>
2024-02-11 11:07:12 +0900

    청첩장 step 4 : 사진 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import Gallery from './Gallery.js'
 import Movie from './Movie.js'
 import CountdownTimer from './CountdownTimer.js'
 import React from 'react';
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 const ImageComponent = ({ num }) => (
   <img src={`/images/${num}.png`} alt="An example image" />
@@ -10,35 +11,47 @@ const ImageComponent = ({ num }) => (
 
 const MemoizedImage = React.memo(ImageComponent);
 
+const images = [];
+
+for (let i = 1; i <= 16; i++) {
+  images.push({
+    original: `/images/gallery/${i}.jpg`,
+    thumbnail: `/images/gallery/thumbnail/${i}.jpg`,
+  });
+}
 
 function App() {
   return (
     <div className="container">
       {/* 사진 추가 */}
       <CountdownTimer></CountdownTimer>
-      <div className="image-wrapper">
-        <MemoizedImage num={1}></MemoizedImage>
+      <MemoizedImage num={1}></MemoizedImage>
+      <MemoizedImage num={2}></MemoizedImage>
+      <MemoizedImage num={3}></MemoizedImage>
+      <MemoizedImage num={4}></MemoizedImage>
+      <div className="title">
+        GALLERY
       </div>
-      <div className="image-wrapper">
-        <MemoizedImage num={2}></MemoizedImage>
+      <div className='sub-title'>
+        사랑으로 가득 채운 셀프 촬영 모음집
       </div>
-      <div className="image-wrapper">
-        <MemoizedImage num={3}></MemoizedImage>
-      </div>
-      <div className="image-wrapper">
-        <MemoizedImage num={4}></MemoizedImage>
-      </div>
-
-      <Gallery></Gallery>
+      <ImageGallery items={images} showFullscreenButton={true} showBullets={true} showPlayButton={false} thumbnailPosition='top' />
       <Movie></Movie>
 
-      <div className="image-wrapper">
-        <MemoizedImage num={5}></MemoizedImage>
+      <MemoizedImage num={5}></MemoizedImage>
+
+      <div className="title">
+        LOCATION
       </div>
-      <div className="image-wrapper">
-        <MemoizedImage num={6}></MemoizedImage>
+      <MemoizedImage num={6}></MemoizedImage>
+      <a href='https://map.naver.com/p/entry/place/37000810?lng=126.4654499&lat=33.4604171&placePath=%2F&entry=plt&searchType=place&c=12.02,0,0,0,dh' target='_blank'>
+        <MemoizedImage num={7}></MemoizedImage>
+      </a>
+      <div className="title">
+        CONTACT
       </div>
-    </div>
+      <MemoizedImage num={8}></MemoizedImage>
+    </div >
   );
 }
 
